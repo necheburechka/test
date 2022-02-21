497d749a56c2d013f68eb12df26fb74e3de063be
necheburechka <2020102157@pnu.edu.ru>
2022-02-21 23:26:57 +1000

    first commit

---
+++ test/test.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main()
+{
+
+    int mas[10], i, m = 0, max;
+    std::cout << " Введите 10 элементов массива: ";
+    for (i = 0; i < 10; i++)
+        std::cin >> mas[i];
+    max = mas[0];
+
+    for (i = 1; i < 10; i++) {
+        if (mas[i] > max) {
+            max = mas[i];
+            m = i;
+        }
+    }
+    std::cout << "Максимальный элемент массива: mas[" << m << "] = " << max;
+}
