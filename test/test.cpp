f8b3794097f4de56db53474cd70585aecd6430cf
necheburechka <2020102157@pnu.edu.ru>
2022-02-22 21:00:38 +1000

    name variable

--- test/test.cpp
+++ test/test.cpp
@@ -4,15 +4,15 @@
 int main()
 {
 
-    int mas[10], i, m = 0, max;
+    int a[10], i, m = 0, max;
     std::cout << " Введите 20 элементов массива: ";
     for (i = 0; i < 10; i++)
-        std::cin >> mas[i];
-    max = mas[0];
+        std::cin >> a[i];
+    max = a[0];
 
     for (i = 1; i < 10; i++) {
-        if (mas[i] > max) {
-            max = mas[i];
+        if (a[i] > max) {
+            max = a[i];
             m = i;
         }
     }
