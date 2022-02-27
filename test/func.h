70113d809064ccee7c774d347b213e07661493d4
necheburechka <2020102157@pnu.edu.ru>
2022-02-27 12:05:07 +1000

    +doxygen

--- test/func.h
+++ test/func.h
@@ -1,11 +1,36 @@
-
 #ifndef FUNC_H
 #define FUNC_H
  
-
+/**
+* \brief This function ADD calculates 
+* the sum of two argumentsthis 
+* function calculates the sum of two arguments
+*
+* Execution of the program
+* starts here.
+* 
+* \param{int} x The first argument
+* \param{int} y The second argument
+* 
+* \return{int} The sum of two numbers passed as arguments
+*/
 int add(int x, int y)
 {
     return x + y;
 }
  
 #endif
+
+/*! 
+ * \brief add function.
+ *
+ * Details about add function.
+ */
+
+/*! \fn int *add(int x,int y) 
+ *  \brief A example function.
+ *  \param x an integer.
+ *  \param y an integer.
+ *  \exception std::out_of_range parameter is out of range.
+ *  \return a sum of x and y.
+ */
--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,27 @@
-﻿#include <iostream>
+﻿/** @defgroup group1 The First Group
+ *  This is the first group
+ *  @{
+ */
+
+#include <iostream>
 #include "func.h"
 
+/** @brief class Calculator, in group 1
+ * 
+ * 
+ * 
+ * In latest version i hope to add class sinature, that will be used to create my own console calculator
+ */
+class Calculator {};
+
+
+/** 
+ * Main of function, entry point
+ * 
+ * In execution you need to input to integer, and program show sum of these numbers
+ * 
+ * @return {int} exitcode - if return 0, then program was executed succesfull 
+ */
 int main()
 {
 	int x, y;
@@ -8,3 +29,5 @@ int main()
     std::cout << "The sum is " << add(x, y) << std::endl;
     return 0;
 }
+
+/** @} */ // end of group1
